8688fe5fbaab09bc0d5d546cd49adcad3f350341
Your Name <your-email@example.com>
2024-03-24 04:40:32 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+cats = ["Milo", "Otis", "Garfield"]
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  })
+
+
+  function destructivelyAppendCat(name) {
+    cats.push(name)
+    name = "Ralph"
+  }
+
+
+  function destructivelyPrependCat(name){
+    cats.unshift(name)
+    name = "Bob"
+  }
+
+  function destructivelyRemoveLastCat(){
+    cats.pop()
+  }
+
+  function destructivelyRemoveFirstCat(){
+    cats.shift()
+  }
+
+  function appendCat(name){
+    name = [...cats,name]
+    name = "Broom"
+  }
+
+  function prependCat(name){
+    name = [name,...cats]
+    name = "Arnold"
+  }
+
+  function removeLastCat(){
+    cats.slice(-1,-2)
+  }
+
+  function removeFirstCat(){
+    cats.slice(0,1)
+  }
+
+  
